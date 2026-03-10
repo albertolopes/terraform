527062d337286c393946a40d1b110b92d87ba186
beto <beto@localhost>
2026-03-10 09:31:28 -0300

    fix: adicionando certificado tls

--- modules/nginx/main.tf
+++ modules/nginx/main.tf
@@ -3,6 +3,49 @@ variable "domain_name" {
   type        = string
 }
 
+# --- Geração de Certificado Auto-assinado ---
+resource "tls_private_key" "nginx" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "tls_self_signed_cert" "nginx" {
+  private_key_pem = tls_private_key.nginx.private_key_pem
+
+  subject {
+    common_name  = "*.${var.domain_name}"
+    organization = "Avocado Tech"
+  }
+
+  validity_period_hours = 8760 # 1 ano
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+
+  dns_names = [
+    var.domain_name,
+    "*.${var.domain_name}",
+    "nginx.${var.domain_name}",
+    "keycloak.${var.domain_name}",
+    "minio.${var.domain_name}"
+  ]
+}
+
+resource "kubernetes_secret_v1" "nginx_certs" {
+  metadata {
+    name      = "nginx-certs"
+    namespace = "default"
+  }
+  type = "kubernetes.io/tls"
+  data = {
+    "tls.crt" = tls_self_signed_cert.nginx.cert_pem
+    "tls.key" = tls_private_key.nginx.private_key_pem
+  }
+}
+
 resource "kubernetes_config_map_v1" "nginx_config" {
   metadata {
     name      = "nginx-config"
@@ -28,9 +71,16 @@ resource "kubernetes_config_map_v1" "nginx_config" {
         # Resolver interno do K8s
         resolver 10.43.0.10 valid=30s;
 
+        # Configurações SSL Globais
+        ssl_protocols TLSv1.2 TLSv1.3;
+        ssl_prefer_server_ciphers on;
+        ssl_certificate /etc/nginx/certs/tls.crt;
+        ssl_certificate_key /etc/nginx/certs/tls.key;
+
         # Keycloak Proxy
         server {
           listen 80;
+          listen 443 ssl;
           server_name keycloak.${var.domain_name};
 
           location / {
@@ -38,10 +88,9 @@ resource "kubernetes_config_map_v1" "nginx_config" {
             proxy_set_header Host $host;
             proxy_set_header X-Real-IP $remote_addr;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto https;
-            proxy_set_header X-Forwarded-Port 443;
-            proxy_set_header X-Forwarded-Ssl on;
+            proxy_set_header X-Forwarded-Proto $scheme;
             proxy_set_header X-Forwarded-Host $host;
+            proxy_set_header X-Forwarded-Port $server_port;
 
             # Forçar redirecionamentos serem relativos ao host acessado
             proxy_redirect http://$host/ /;
@@ -53,6 +102,7 @@ resource "kubernetes_config_map_v1" "nginx_config" {
         # Minio Proxy
         server {
           listen 80;
+          listen 443 ssl;
           server_name minio.${var.domain_name};
 
           location / {
@@ -60,9 +110,7 @@ resource "kubernetes_config_map_v1" "nginx_config" {
             proxy_set_header Host $host;
             proxy_set_header X-Real-IP $remote_addr;
             proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto https;
-            proxy_set_header X-Forwarded-Port 443;
-            proxy_set_header X-Forwarded-Ssl on;
+            proxy_set_header X-Forwarded-Proto $scheme;
             proxy_set_header X-Forwarded-Host $host;
 
             # Redirecionamentos para Minio
@@ -75,6 +123,7 @@ resource "kubernetes_config_map_v1" "nginx_config" {
         # Nginx (Default / Root)
         server {
           listen 80;
+          listen 443 ssl;
           server_name nginx.${var.domain_name} ${var.domain_name} _;
 
           location / {
@@ -139,7 +188,12 @@ resource "kubernetes_deployment_v1" "nginx" {
           name  = "nginx"
           image = "nginx:1.29.4-alpine"
           port {
+            name           = "http"
             container_port = 80
+          }
+          port {
+            name           = "https"
+            container_port = 443
           }
           resources {
             requests = {
@@ -160,6 +214,11 @@ resource "kubernetes_deployment_v1" "nginx" {
             mount_path = "/etc/nginx/nginx.conf"
             sub_path   = "nginx.conf"
           }
+          volume_mount {
+            name       = "nginx-certs"
+            mount_path = "/etc/nginx/certs"
+            read_only  = true
+          }
         }
         volume {
           name = "nginx-config"
@@ -171,6 +230,12 @@ resource "kubernetes_deployment_v1" "nginx" {
           name = "html-config"
           config_map {
             name = kubernetes_config_map_v1.nginx_html.metadata[0].name
+          }
+        }
+        volume {
+          name = "nginx-certs"
+          secret {
+            secret_name = kubernetes_secret_v1.nginx_certs.metadata[0].name
           }
         }
       }
@@ -196,7 +261,7 @@ resource "kubernetes_service_v1" "nginx" {
     port {
       name        = "https"
       port        = 443
-      target_port = 80
+      target_port = 443
     }
   }
 }
