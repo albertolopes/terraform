dd18efe6260e81a4c3b753e0dbae4fd4e06a2708
beto <beto@localhost>
2026-04-14 16:23:38 -0300

    fix: retirando providers main.tf do gitlab

--- modules/gitlab/main.tf
+++ modules/gitlab/main.tf
@@ -1,18 +1,5 @@
 # modules/gitlab/main.tf
 
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = ">= 2.0.0"
-    }
-  }
-}
-
 resource "kubernetes_namespace_v1" "gitlab" {
   metadata {
     name = var.namespace
