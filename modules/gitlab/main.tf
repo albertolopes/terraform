37e9e8c3d664a753634354f34fcc599f1f33d786
beto <beto@localhost>
2026-04-15 12:19:39 -0300

    feat: adicionando mais recursos ao gitlab

--- modules/gitlab/main.tf
+++ modules/gitlab/main.tf
@@ -126,11 +126,21 @@ resource "helm_release" "gitlab" {
         maxReplicas: 1
         resources:
           requests:
-            cpu: 100m
-            memory: 256Mi
+            cpu: 300m
+            memory: 1Gi
           limits:
-            cpu: 500m
-            memory: 512Mi
+            cpu: 1500m
+            memory: 2Gi
+        livenessProbe:
+          initialDelaySeconds: 300
+          periodSeconds: 10
+          timeoutSeconds: 5
+          failureThreshold: 3
+        readinessProbe:
+          initialDelaySeconds: 240
+          periodSeconds: 5
+          timeoutSeconds: 3
+          failureThreshold: 2
 
       sidekiq:
         minReplicas: 1
