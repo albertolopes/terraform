89a4c4f9c76db83e74af55bcc83138ca430a8b7b
beto <beto@localhost>
2026-04-27 12:04:22 -0300

    fix: cloudflare

--- modules/cloudflare/main.tf
+++ modules/cloudflare/main.tf
@@ -61,20 +61,21 @@ resource "cloudflare_record" "tunnel_cnames" {
 }
 
 # 5. Forçar SSL e HTTPS na Cloudflare via Terraform
-resource "cloudflare_zone_settings_override" "zone_settings" {
-  zone_id = var.cloudflare_zone_id
-
-  settings {
-    # Modo de criptografia SSL (Full/Strict)
-    ssl = "strict"
-    
-    # Redirecionar todo HTTP para HTTPS na borda da Cloudflare
-    always_use_https = "on"
-    
-    # Otimizações de segurança recomendadas
-    min_tls_version = "1.2"
-  }
-}
+# (Comentado devido a erro de 'prefetch_preload' em planos Free/Pro)
+# resource "cloudflare_zone_settings_override" "zone_settings" {
+#   zone_id = var.cloudflare_zone_id
+# 
+#   settings {
+#     # Modo de criptografia SSL (Full/Strict)
+#     ssl = "strict"
+#     
+#     # Redirecionar todo HTTP para HTTPS na borda da Cloudflare
+#     always_use_https = "on"
+#     
+#     # Otimizações de segurança recomendadas
+#     min_tls_version = "1.2"
+#   }
+# }
 
 # 6. Criar o segredo do túnel no Kubernetes
 resource "kubernetes_secret_v1" "tunnel_token" {
