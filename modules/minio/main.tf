948dff07ab787781c3e4d2970510324741acd469
beto <beto@localhost>
2026-05-02 14:59:12 -0300

    fix minio

--- modules/minio/main.tf
+++ modules/minio/main.tf
@@ -56,7 +56,7 @@ resource "kubernetes_deployment_v1" "minio" {
       spec {
         container {
           name  = "minio"
-          image = "minio/minio:RELEASE.2026-04-17T00-00-00Z"
+          image = "minio/minio:latest"
           args  = ["server", "/data", "--console-address", ":9001"]
 
           liveness_probe {
