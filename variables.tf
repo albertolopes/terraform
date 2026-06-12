15fbea7536c170d166c96177fd4910045e23c18e
beto <beto@localhost>
2026-06-12 15:24:59 -0300

    fix

--- variables.tf
+++ variables.tf
@@ -49,6 +49,37 @@ variable "postgres_password" {
   default = "postgres"
 }
 
+variable "pgadmin_hostname" {
+  description = "Subdominio publico do pgAdmin."
+  type        = string
+  default     = "pgadmin"
+}
+
+variable "pgadmin_email" {
+  description = "Email inicial de login do pgAdmin."
+  type        = string
+  default     = "admin@avocadotech.site"
+}
+
+variable "pgadmin_password" {
+  description = "Senha inicial de login do pgAdmin."
+  type        = string
+  sensitive   = true
+  default     = "pgadmin123"
+}
+
+variable "pgadmin_storage_size" {
+  description = "Tamanho do volume persistente do pgAdmin."
+  type        = string
+  default     = "2Gi"
+}
+
+variable "pgadmin_enable_ingress" {
+  description = "Habilita Ingress publico para o pgAdmin."
+  type        = bool
+  default     = true
+}
+
 variable "gitlab_runner_token" {
   description = "GitLab Runner authentication token"
   type        = string
